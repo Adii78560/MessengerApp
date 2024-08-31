0abe5564644f5a657fcd0c52c970c7693624ca6b
Adii78560 <raiaditya145@gmail.com>
2024-09-01 00:51:14 +0530

    test1

---
+++ MessengerApp/ Core/Authentication/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  MessengerApp
+//
+//  Created by Aditya Rai on 01/09/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationStack{
+            
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
