0b16d5de044859bd2a35c8d475c876eaf014a067
Adii78560 <raiaditya145@gmail.com>
2024-09-01 00:25:08 +0530

    Initial Commit

---
+++ MessengerApp/MessengerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerAppApp.swift
+//  MessengerApp
+//
+//  Created by Aditya Rai on 01/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MessengerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
